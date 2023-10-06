d55f9ff6fb7df8b18906c5fb14bdd1863d6b05ec
Keiseker <juliamine1538@gmail.com>
2023-10-06 11:44:01 +0300

    The layout of the auth page

--- lib/features/auth/auth_screen.dart
+++ lib/features/auth/auth_screen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'auth_tabs/login_tab.dart';
+import 'auth_tabs/registration_tab.dart';
 
 class AuthScreen extends StatefulWidget {
   const AuthScreen({super.key});
@@ -10,6 +12,30 @@ class AuthScreen extends StatefulWidget {
 class _AuthScreenState extends State<AuthScreen> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return const DefaultTabController(
+        length: 2,
+        child: Scaffold(
+            body: Column(
+          children: [
+            SizedBox(height: 14,),
+            TabBar(tabs: [
+              Tab(
+                child: Text('Регистрация',style: TextStyle(color: Color(0xff0F172A),fontSize: 14),),
+              ),
+              Tab(
+                child: Text('Вход',style: TextStyle(color: Color(0xff0F172A),fontSize: 14),),
+              )
+            ]),
+            Expanded(
+              flex: 2,
+              child: TabBarView(
+                children: [
+                  RegistrationTab(),
+                  LoginTab(),
+                ],
+              ),
+            )
+          ],
+        )));
   }
 }
