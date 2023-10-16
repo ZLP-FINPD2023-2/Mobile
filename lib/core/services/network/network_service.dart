25639e052279607d03354d00dbfbffc8df0326b5
f1xgun <msnzlmm@mail.ru>
2023-10-16 13:06:16 +0300

    feat: add network service and env

---
+++ lib/core/services/network/network_service.dart
@@ -0,0 +1,34 @@
+import 'package:dio/dio.dart';
+import 'package:fin_app/core/flutter_core.dart';
+
+class NetworkService {
+  final Dio _dio;
+
+  const NetworkService({required Dio dio}) : _dio = dio;
+
+  Future<AppStateSuccess<T>> delete<T, K>(
+      String endpoint, Function(K data)? mapFunction) {
+    return NetworkHandler.parseResult<T, K>(_dio.delete(endpoint), mapFunction);
+  }
+
+  Future<AppStateSuccess<T>> get<T, K>(
+      String endpoint, Function(K data)? mapFunction,
+      {Map<String, dynamic>? queryParameters}) {
+    return NetworkHandler.parseResult<T, K>(
+        _dio.get(endpoint, queryParameters: queryParameters), mapFunction);
+  }
+
+  Future<AppStateSuccess<T>> post<T, K>(
+      String endpoint, Function(K data)? mapFunction,
+      {Map<String, dynamic>? queryParameters}) {
+    return NetworkHandler.parseResult<T, K>(
+        _dio.post(endpoint, queryParameters: queryParameters), mapFunction);
+  }
+
+  Future<AppStateSuccess<T>> put<T, K>(
+      String endpoint, Function(K data)? mapFunction,
+      {Map<String, dynamic>? queryParameters}) {
+    return NetworkHandler.parseResult<T, K>(
+        _dio.put(endpoint, queryParameters: queryParameters), mapFunction);
+  }
+}
