36ce36f00403dee0a88fe0a39c83da17e8cdf8ae
keiseker <keiseker7@yandex.ru>
2023-11-25 20:20:36 +0300

    Add item_budget

--- lib/features/home/presentation/budget/budget_screen.dart
+++ lib/features/home/presentation/budget/budget_screen.dart
@@ -1,7 +1,7 @@
 import 'package:fin_app/constants/colors.dart';
 import 'package:fin_app/core/extensions/context.dart';
 import 'package:fin_app/features/home/presentation/budget/widgets/add_budget.dart';
-import 'package:fin_app/features/home/presentation/budget/widgets/budget_info.dart';
+import 'package:fin_app/features/home/presentation/budget/widgets/item_budget.dart';
 import 'package:flutter/material.dart';
 
 class BudgetInfo {
@@ -51,46 +51,9 @@ class _NavigationExampleState extends State<BudgetScreen> {
         child: ListView.builder(
             itemCount: listOfBudgets.length,
             itemBuilder: (context, index) {
-              return GestureDetector(
-                  onTap: () {
-                    Navigator.push(
-                      context,
-                      MaterialPageRoute(
-                        builder: (context) => BudgetInfoScreen(index: index),
-                      ),
-                    );
-                  },
-                  child: Padding(
-                      padding: const EdgeInsets.fromLTRB(16, 12, 16, 12),
-                      child: Row(
-                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                          children: [
-                            Row(
-                              children: [
-                                Container(
-                                  height: 15.0,
-                                  width: 15.0,
-                                  decoration: const BoxDecoration(
-                                    color: walletColor,
-                                    shape: BoxShape.circle,
-                                  ),
-                                ),
-                                const SizedBox(
-                                  width: 5,
-                                ),
-                                Text(listOfBudgets[index].name,
-                                    style: const TextStyle(
-                                        fontSize: 18,
-                                        color: Colors.black,
-                                        fontWeight: FontWeight.w700)),
-                              ],
-                            ),
-                            Text("${listOfBudgets[index].sum}₽",
-                                style: const TextStyle(
-                                    fontSize: 18,
-                                    color: Colors.black,
-                                    fontWeight: FontWeight.w700))
-                          ])));
+              return ItemBudget(
+                index: index,
+              );
             }),
       ),
     );
---
+++ lib/features/home/presentation/budget/widgets/item_budget.dart
@@ -0,0 +1,56 @@
+import 'package:flutter/material.dart';
+import 'package:fin_app/features/home/presentation/budget/widgets/budget_info.dart';
+import 'package:fin_app/features/home/presentation/budget/budget_screen.dart';
+import 'package:fin_app/constants/colors.dart';
+
+class ItemBudget extends StatelessWidget {
+  final int index;
+  const ItemBudget({
+    required this.index,
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+        onTap: () {
+          Navigator.push(
+            context,
+            MaterialPageRoute(
+              builder: (context) => BudgetInfoScreen(index: index),
+            ),
+          );
+        },
+        child: Padding(
+            padding: const EdgeInsets.fromLTRB(16, 12, 16, 12),
+            child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Row(
+                    children: [
+                      Container(
+                        height: 15.0,
+                        width: 15.0,
+                        decoration: const BoxDecoration(
+                          color: walletColor,
+                          shape: BoxShape.circle,
+                        ),
+                      ),
+                      const SizedBox(
+                        width: 5,
+                      ),
+                      Text(listOfBudgets[index].name,
+                          style: const TextStyle(
+                              fontSize: 18,
+                              color: Colors.black,
+                              fontWeight: FontWeight.w700)),
+                    ],
+                  ),
+                  Text("${listOfBudgets[index].sum}₽",
+                      style: const TextStyle(
+                          fontSize: 18,
+                          color: Colors.black,
+                          fontWeight: FontWeight.w700))
+                ])));
+  }
+}
